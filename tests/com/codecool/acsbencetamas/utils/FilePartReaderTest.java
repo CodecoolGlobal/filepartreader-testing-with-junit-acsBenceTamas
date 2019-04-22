aca5ea35d31f51cf957e4e67356042be5628b37e
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-04-22 13:41:22 +0200

    Tests written. Reading fails

--- tests/com/codecool/acsbencetamas/utils/FilePartReaderTest.java
+++ tests/com/codecool/acsbencetamas/utils/FilePartReaderTest.java
@@ -1,7 +1,128 @@
 package com.codecool.acsbencetamas.utils;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilePartReaderTest {
+    private static final String TEST_FILE_PATH = "tests/com/codecool/acsbencetamas/utils/simpleTestFile.txt";
 
+    @Test
+    public void testCanDoValidSetup() {
+        FilePartReader fpr = new FilePartReader();
+
+        fpr.setup(TEST_FILE_PATH,1,1);
+    }
+
+    @Test
+    public void testFromLineLowerThanOneThrowsException() {
+        FilePartReader fpr = new FilePartReader();
+
+        assertThrows(IllegalArgumentException.class,() ->
+                fpr.setup(TEST_FILE_PATH,0,1)
+        );
+    }
+
+    @Test
+    public void testToLineLowerThanFromLineThrowsException() {
+        FilePartReader fpr = new FilePartReader();
+
+        assertThrows(IllegalArgumentException.class,() ->
+                fpr.setup(TEST_FILE_PATH,2,1)
+        );
+    }
+
+    @Test
+    public void testCanReadWholeFile() {
+        FilePartReader fpr = new FilePartReader();
+
+        fpr.setup(TEST_FILE_PATH,1,1);
+        try {
+            assertEquals(fpr.read(),
+                    "line one\n" +
+                            "line two\n" +
+                            "line three\n" +
+                            "line four\n" +
+                            "line five\n" +
+                            "line six\n" +
+                            "line seven\n" +
+                            "line eight\n" +
+                            "line nine\n" +
+                            "line ten");
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCanReadFirstLine() {
+        FilePartReader fpr = new FilePartReader();
+
+        fpr.setup(TEST_FILE_PATH,1,1);
+        try {
+            assertEquals(fpr.read(),
+                    "line one");
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCanReadSingleLineInMiddle() {
+        FilePartReader fpr = new FilePartReader();
+
+        fpr.setup(TEST_FILE_PATH,5,5);
+        try {
+            assertEquals(fpr.read(),
+                    "line five");
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCanReadPartsBeforeLastLine() {
+        FilePartReader fpr = new FilePartReader();
+
+        fpr.setup(TEST_FILE_PATH,2,5);
+        try {
+            assertEquals(fpr.read(),
+                    "line two\n" +
+                            "line three\n" +
+                            "line four\n" +
+                            "line five\n");
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCanReadPartsPastLastLine() {
+        FilePartReader fpr = new FilePartReader();
+
+        fpr.setup(TEST_FILE_PATH,8,15);
+        try {
+            assertEquals(fpr.read(),
+                    "line eight\n" +
+                            "line nine\n" +
+                            "line ten\n");
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testReadingLinesAfterLastLineReturnsEmptyString() {
+        FilePartReader fpr = new FilePartReader();
+
+        fpr.setup(TEST_FILE_PATH,11,11);
+        try {
+            assertEquals(fpr.read(),
+                    "");
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
+    }
 }
